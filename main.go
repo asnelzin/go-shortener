45dae79257fceaf6ce3a407f8fdaddea6de3835f
Alexander Nelzin <asnelzin@gmail.com>
2015-03-04 14:15:55 +0300

    Cleanup, move redis API to core.go

--- main.go
+++ main.go
@@ -2,38 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/garyburd/redigo/redis"
+	// "github.com/go-martini/martini"
 	"github.com/gorilla/mux"
 	"net/http"
 )
-
-var (
-	redisConn *redis.Conn
-)
-
-func urlHash(conn redis.Conn) string {
-	if key, err := conn.Do("INCR", "url.pointer"); err != nil {
-		panic(err)
-	} else {
-		return fmt.Sprintf("%x", key)
-	}
-}
-
-func createRecord(conn redis.Conn, url string) string {
-	hash := urlHash(conn)
-	if _, err := conn.Do("SET", hash, url); err != nil {
-		panic(err)
-	}
-	return hash
-}
-
-func getUrl(conn redis.Conn, hash string) string {
-	if url, err := conn.Do("GET", hash); err != nil {
-		panic(err)
-	} else {
-		return fmt.Sprintf("%s", url)
-	}
-}
 
 func index(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -49,15 +21,11 @@ func index(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// conn, err := redis.Dial("tcp", ":6379")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer conn.Close()
+	redisApi := NewRedisApi("localhost", "6379")
+	fmt.Println(redisApi.createRecord("http://localhost"))
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", index).Methods("GET", "POST")
-	// router.HandleFunc("path", url)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":3000", nil)
