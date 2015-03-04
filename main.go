b2da12009acdfc9b6d7210205991a87f2c033f33
Alexander Nelzin <asnelzin@gmail.com>
2015-03-04 23:24:00 +0300

    Add martini, basic functionality

--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/go-martini/martini"
-	"github.com/gorilla/mux"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 	"net/http"
 )
 
-func index(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Write([]byte("Hello World"))
-	case "POST":
-		params := mux.Vars(r)
-		rw.Write([]byte(r.FormValue("url")))
-		rw.Write([]byte(params["url"]))
-	default:
-		panic("Not implemented")
-	}
-}
+type JsonResponse map[string]interface{}
+
+type Storage interface{}
 
 func main() {
-	redisApi := NewRedisApi("localhost", "6379")
-	fmt.Println(redisApi.createRecord("http://localhost"))
+	var storage Storage
+	redisStorage := NewRedisApi("localhost", "6379") // add config
+	storage = redisStorage
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", index).Methods("GET", "POST")
+	api := martini.Classic()
+	api.Use(render.Renderer(render.Options{
+		IndentJSON: true,
+	}))
+	api.MapTo(storage, (*Storage)(nil))
 
-	http.Handle("/", router)
-	http.ListenAndServe(":3000", nil)
+	api.Post("/", func(r render.Render, storage Storage, req *http.Request) {
+		if url := req.FormValue("url"); url != "" {
+			hash := redisStorage.createRecord(url)
+			response := JsonResponse{
+				"shortUrl": hash,
+			}
+			r.JSON(http.StatusOK, response)
+		}
+	})
+
+	api.Run()
 }
