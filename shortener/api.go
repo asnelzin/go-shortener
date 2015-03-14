2f43e8e833c3ebd9bd68a4994423160dd885ceba
Alexander Nelzin <asnelzin@gmail.com>
2015-03-14 18:27:06 +0300

    Update routers for deploy

--- shortener/api.go
+++ shortener/api.go
@@ -34,11 +34,11 @@ func GetApi() *martini.ClassicMartini {
 	}
 	api.MapTo(storage, (*Storage)(nil))
 
-	api.Get("/", func(r render.Render) {
+	api.Get("/s", func(r render.Render) {
 		r.HTML(200, "index", nil)
 	})
 
-	api.Post("/", func(r render.Render, storage Storage, req *http.Request) {
+	api.Post("/s", func(r render.Render, storage Storage, req *http.Request) {
 		if url := req.FormValue("url"); url != "" {
 			if govalidator.IsURL(url) {
 				if !strings.HasPrefix(url, "http") {
@@ -55,7 +55,7 @@ func GetApi() *martini.ClassicMartini {
 		}
 	})
 
-	api.Get("/(?P<hash>[a-zA-Z0-9]+)", func(r render.Render, params martini.Params, storage Storage, req *http.Request) {
+	api.Get("/s/(?P<hash>[a-zA-Z0-9]+)", func(r render.Render, params martini.Params, storage Storage, req *http.Request) {
 		if url, err := storage.GetUrl(params["hash"]); err != nil {
 			r.JSON(http.StatusNotFound, JsonResponse{"error": err.Error()})
 		} else {
